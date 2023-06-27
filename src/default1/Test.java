07989864b2545815e790900bf9fbbd10675653be
new <new@DESKTOP-R54M34O>
2023-06-27 14:54:28 +0530

    commit

---
+++ src/default1/Test.java
@@ -0,0 +1,16 @@
+package default1;
+
+public class Test {
+	public static void main(String[] args) {
+		String hungry="yes";
+				
+				if(hungry == "yes")
+					System.out.println("eat something......");
+				else {
+					System.out.println("Do work.......");
+				}
+
+			}
+
+	
+}
